5922343953d6d6a0383b441406ab2d290156b139
Datebayo350 <istrateconstantin6@gmail.com>
2022-03-07 14:58:19 +0100

    rootReducer component created, global store initialised above the App component into the index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './_reducers_/rootReducer';
+
+const store = createStore(
+  rootReducer,
+    applyMiddleware(
+      thunkMiddleware
+    ),
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
