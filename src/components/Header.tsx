0ddcdd0950fea6ea4aac0c77fd9579750abce0e2
Datebayo350 <istrateconstantin6@gmail.com>
2022-03-08 17:14:54 +0100

    Navbar functional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,33 @@
 import { NavLink } from 'react-router-dom';
+import { protectedRoutes, unProtectedRoutes } from './../utils/routes';
 
-const Header = (userIsAuth) => {
-    
-    const navBar = (authenticatedRoutes) => {
-        
-        authenticatedRoutes.map((route, index) => {
+interface HeaderProps {
+    userIsAuth: boolean;
+}
+
+const Header = ({userIsAuth}: HeaderProps) => {
+    const navBar = () => {
+
+        let routesToDisplay;
+        routesToDisplay = userIsAuth ? protectedRoutes : unProtectedRoutes;
+
+        return  routesToDisplay.map((route) => {
+            
             return (
-                <NavLink key={index} to={route.path}>{route.name}</NavLink>
+                <li>
+                    <NavLink key={route.key} to={route.path} className={ ({ isActive }) => isActive ? "bg-amber-400" : "bg-amber-100"}>{route.name}</NavLink>
+                </li>
             )
-        }
+        })
     };
+
     return (
         <>
             <ul>
-                {navBar(authenticatedRoutes)}
+                {navBar()}
             </ul>
         </>
     )
 }
 
-export default Header
+export default Header;
