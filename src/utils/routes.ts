ad093b3434b654fbf586e232e1a239d7f645b061
Datebayo350 <istrateconstantin6@gmail.com>
2022-03-08 17:19:15 +0100

    Some files / directory renamed and the Home page is now accessible by anonymous user

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,18 +1,18 @@
 interface routes {
     key: string,
     path: string,
+    name: string,
     element: string,
-    exact: boolean,
     routes?: routes[]
 }
 
 export const protectedRoutes: routes[] = [
 
-    {key: "MA SELECTION", path: "/ma-selection", element: "MySelection", exact: true},
-    {key: "CORBEILLE", path: "/corbeille", element: "WasteBin", exact: true},
+    {key: "MA SELECTION", path: "/ma-selection", name: "ma sélection", element: "MySelection"},
+    {key: "CORBEILLE", path: "/corbeille", name: "corbeille", element: "WasteBin"},
 ]
 
 export const unProtectedRoutes: routes[] = [
     
-    {key: "ACCUEIL", path: "/", element: "Home", exact: true},
+    {key: "ACCUEIL", path: "/", name: "accueil", element: "Home"},
 ]
