d55f1b47ba460c2c0e29bce1d100303c7473ff68
Datebayo350 <istrateconstantin6@gmail.com>
2022-03-11 10:07:24 +0100

    Header component creation (operational)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { NavLink } from 'react-router-dom';
+import Button from './dsfrComponents/Button';
 import { protectedRoutes, unProtectedRoutes } from './../utils/routes';
-
+import {LightTrash, LightStar} from './../assets/Icons';
+import LogoMSG from './../assets/msg-icon.png';
+import LogoMSGBeta from './../assets/icon-msg-txt-beta.png';
 interface HeaderProps {
     userIsAuth: boolean;
 }
@@ -12,21 +15,100 @@ const Header = ({userIsAuth}: HeaderProps) => {
         routesToDisplay = userIsAuth ? protectedRoutes : unProtectedRoutes;
 
         return  routesToDisplay.map((route) => {
-            
+            let routElementBooleanValue:boolean;
+            routElementBooleanValue = typeof route.element === 'boolean';
+
             return (
-                <li key={route.key}>
-                    <NavLink key={route.key} to={route.path} className={ ({ isActive }) => isActive ? "bg-amber-400" : "bg-amber-100"}>{route.name}</NavLink>
-                </li>
+                <>
+                {routElementBooleanValue ?
+                    // TODO: Methode avec le <Button> à utiliser après - voir comment régler l'erreur
+                    // <Button route={...route}/>
+
+                    <button className="fr-btn fr-btn--secondary
+                        h-full my-auto
+                        hover:bg-claire-bf__hover
+                    "> {route.name } </button> 
+                    :
+                    <li key={route.key} className="fr-nav__item h-full w-full p-0">
+                        
+                        <NavLink 
+                            key={route.key}
+                            to={route.path} 
+                            className="fr-nav__link"
+                            > 
+                                <div className="flex">
+                                    {route.key === 'SELECTION' ?
+                                    
+                                        <span className="h-full my-auto mr-2">
+                                            <LightStar/>
+                                        </span>
+                                    
+                                        :
+                                        <LightTrash/>                               
+                                    }
+                                    <div className="flex flex-col">
+                                        <p className="font-bold"> {route.name} </p>
+                                        <p className="text-xs"> X pistes </p>
+                                    </div>
+                                </div>
+
+                        </NavLink>
+                    </li>
+                }
+                </>
             )
         })
     };
 
     return (
-        <>
-            <ul>
-                {navBar()}
-            </ul>
-        </>
+        <header role="banner" className="fr-header">
+
+            <div className="fr-header__body">
+                <div className="fr-container">
+                    <div className="fr-header__body-row">
+                        <div className="fr-header__brand fr-enlarge-link">
+                            <div className="fr-header__brand-top">
+                                <div className="fr-header__logo">
+                                    <p className="fr-logo uppercase">
+                                        ministère <br/>
+                                        de la transition <br/>
+                                        ecologique
+                                    </p>
+                                </div>
+                                <div className="fr-header__navbar">
+                                    <button className="fr-btn--menu fr-btn" data-fr-opened="false" aria-controls="modal-833" aria-haspopup="menu" title="Menu" id="fr-btn-menu-mobile">
+                                        Menu
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="fr-header__service">
+                                <a className="flex bg-red-200" href="/" title="Accueil - Mes services GreenTech - Ministère de la transition écologique">
+                                    {/* <img className="h-12 mt-1 mr-2" src={LogoMSG} alt="Icône d'indication, version beta "/> */}
+                                    {/* <p className="fr-header__service-title capitalize ">mes services<br/> greentech</p> */}
+                                    <img className="h-12 mt-1 mr-2" src={LogoMSGBeta} alt="Icône d'indication, version beta "/>
+                                </a>
+                                <p className="fr-header__service-tagline">baseline - précisions sur l‘organisation</p>
+                            </div>
+                        </div>
+                        <div className="fr-header__tools flex justify-end">
+                            <div className="fr-header__tools-links">
+                                <ul className="fr-nav__list">
+                                    {navBar()}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="fr-header__menu fr-modal" id="modal-833" aria-labelledby="fr-btn-menu-mobile">
+                <div className="fr-container">
+                    <button className="fr-link--close fr-link" aria-controls="modal-833">Fermer</button>
+                    <div className="fr-header__menu-links"></div>
+                </div>
+            </div>
+
+        </header>
     )
 }
 
