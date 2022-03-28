8f7d6c410a2a15c95f612872df3975e7f9eedf46
Datebayo350 <istrateconstantin6@gmail.com>
2022-03-28 15:40:08 +0200

    Pagination structure before improvment

--- src/components/dsfrComponents/Pagination.tsx
+++ src/components/dsfrComponents/Pagination.tsx
@@ -1,12 +1,15 @@
+import Chevron from './../../assets/icons/chevron.svg'
 interface PaginationProps { 
     cursor1: string
     cursor2: string
     cursor3: string
     cursor4: string
-
+    previousPage: string
+    currentPage: string,
+    nextPage: string,
 } 
 
-const Pagination: React.FC<PaginationProps> = ({cursor1, cursor2, cursor3, cursor4}) => { 
+const Pagination: React.FC<PaginationProps> = ({cursor1, cursor2, cursor3, cursor4, previousPage, currentPage, nextPage, }) => { 
 
     return (
 
@@ -15,26 +18,26 @@ const Pagination: React.FC<PaginationProps> = ({cursor1, cursor2, cursor3, curso
             <nav role="navigation" className="fr-pagination" aria-label="Pagination">
                 <ul className="fr-pagination__list">
 
-                    <li>
+                    {/* <li>
                         <a href="#" className="fr-pagination__link fr-pagination__link--first" aria-disabled="true" role="link">
                             Première page
                         </a>
-                    </li>
+                    </li> */}
 
                     <li>
-                        <a href="#" className="fr-pagination__link fr-pagination__link--prev fr-pagination__link--lg-label" aria-disabled="true" role="link">
-                            Page précédente
+                        <a href={previousPage} className="fr-pagination__link fr-pagination__link--prev fr-pagination__link--lg-label" aria-disabled="true" role="link">
+                            Précédent 
                         </a>
                     </li>
 
                     <li>
-                        <a href="#" className="fr-pagination__link" aria-current="page" title="Page 1">
+                        <a href={currentPage} className="fr-pagination__link rounded-full" aria-current="page" title="Page 1">
                             {cursor1}
                         </a>
                     </li>
 
                     <li>
-                        <a href="#" className="fr-pagination__link" aria-current="page" title="Page 1">
+                        <a href={nextPage} className="fr-pagination__link" aria-current="page" title="Page 1">
                             {cursor2}
                         </a>
                     </li>
@@ -52,16 +55,16 @@ const Pagination: React.FC<PaginationProps> = ({cursor1, cursor2, cursor3, curso
                     </li>
 
                     <li>
-                        <a href="#" className="fr-pagination__link fr-pagination__link--next fr-pagination__link--lg-label">
-                            Page suivante
+                        <a href={nextPage} className="fr-pagination__link fr-pagination__link--next fr-pagination__link--lg-label">
+                            Suivant 
                         </a>
                     </li>
 
-                    <li>
-                        <a href="#" className="fr-pagination__link fr-pagination__link--last">
+                    {/* <li>
+                        <a href={lastPage} className="fr-pagination__link fr-pagination__link--last">
                             Dernière page
                         </a>
-                    </li>
+                    </li> */}
                     
                 </ul>
             </nav>  
