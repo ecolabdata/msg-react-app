a89d1920e593944b5229a4fdd740bb8588b76e5a
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2022-04-28 15:20:03 +0200

    Display all field + margin % of vw

--- src/components/customComponents/DetailsAideInno.tsx
+++ src/components/customComponents/DetailsAideInno.tsx
@@ -15,7 +15,10 @@ const CardDetailsInno = (props: { cardData: Aide }) => {
     const displayableFinancers = cardData.financers?.join(" | ") || ""
     const d = cardData.submission_deadline ? new Date(cardData.submission_deadline) : null
     const displayabeSubmissionDeadLine = ("0" + d?.getUTCDate()).slice(-2) + "/" + ("0" + ((d?.getUTCMonth() || 0) + 1)).slice(-2) + "/" + d?.getUTCFullYear()
-
+    const subvention = handleSubventionDisplay(
+        cardData["subvention_rate_lower_bound"],
+        cardData["subvention_rate_upper_bound"]
+    )
     return (
         <>
             <div  style={{marginLeft: "calc(max(10% - 100px, 0px))", marginRight: "calc(max(10% - 100px, 50px))"}} className="headContainer">
@@ -52,23 +55,37 @@ const CardDetailsInno = (props: { cardData: Aide }) => {
 
             </div>
             <div style={{marginRight: "calc(max(10% - 100px, 0px))", marginLeft: "calc(max(10% - 100px, 0px))"}} className='flex justify-around flex-wrap'>
-                <div style={{flex: "1 1 350px"}} className="contentContainer flex flex-wrap justify-left m-4" >
+                <div style={{flex: "1 1 350px"}} className="contentContainer flex flex-wrap justify-left mx-[5vw]" >
                     <SmallFields color={cardType.color} fieldname={"Calendrier"}>
                         Date de clôture: {cardData.submission_deadline ? `${displayabeSubmissionDeadLine}` : "Aide permanente"}
                     </SmallFields>
                     <SmallFields color={cardType.color} fieldname={"Bénéficiaires"}>
-                        Date de clôture: {cardData.submission_deadline ? `${displayabeSubmissionDeadLine}` : "Aide permanente"}
+                        <ul>
+                            {cardData.targeted_audiences.map(x => <li>{x}</li>)}
+                        </ul>
                     </SmallFields>
                     <SmallFields color={cardType.color} fieldname={"Zone géographique couverte"}>
-                        Date de clôture: {cardData.submission_deadline ? `${displayabeSubmissionDeadLine}` : "Aide permanente"}
+                        {cardData["perimeter"]}
                     </SmallFields>
+                    <BigFields color={cardType.color} fieldname="Critères d’éligibilité">
+                        <span className='dangerouslySetInnerHTML' dangerouslySetInnerHTML={{ __html: cardData["eligibility"] + "" }}></span>
+                    </BigFields>
+                    <BigFields color={cardType.color} fieldname="Description">
+                        <span className='dangerouslySetInnerHTML' dangerouslySetInnerHTML={{ __html: cardData["description"] + "" }}></span>
+                    </BigFields>
+                    <SmallFields color={cardType.color} fieldname={"Nature de l’aide"}>
+                        {cardData.aid_types.join(" | ")}
+                    </SmallFields>
+                    {subvention.display && <SmallFields color={cardType.color} fieldname={"Taux de subvention"}>
+                        {subvention.txt}
+                    </SmallFields>}
                 </div>
                 <div style={{flex: "0 0 380px"}} className="contactCard h-fit rounded addBorder border-2 p-6 border-dark-text-action flex flex-col items-start">
 
 
                     <h2 className="text-[22px] font-bold text-dark-text-action ">Contact</h2>
 
-                    <p className="text-base w-[280px]">{cardData.contact}</p>
+                    <p className="text-base w-[280px]"> <span dangerouslySetInnerHTML={{ __html: cardData["contact"] + "" }}></span></p>
 
                     <a
                         href={cardData["origin_url"]}
@@ -77,7 +94,9 @@ const CardDetailsInno = (props: { cardData: Aide }) => {
                                 w-fit px-4 h-3 py-2
                                 hover:bg-claire-bf__hover
                                 mt-6 
-                                ">
+                                "
+                        target="_blank"
+                    >
                         <span className="fr-fi-phone-fill w-3 h-3 mb-[10px]" aria-hidden="true" />
                         <span className="mt-1 ml-4 text-base">Liens vers l'aide originale</span>
                     </a>
@@ -100,10 +119,29 @@ const CardDetailsInno = (props: { cardData: Aide }) => {
     )
 };
 
-const SmallFields : React.FC<{color: string, fieldname : ReactNode}> = ({color, fieldname, children}) => <div style={{flex: "1 1 auto"}} className='short-field mr-20 mt-8'>
+function  handleSubventionDisplay(min?: number | null, max?: number | null) {
+    const minMax = []
+    min && minMax.push(`Min: ${min}`)
+    max && minMax.push(`Min: ${max}`)
+    return {
+        display: minMax.length || undefined,
+        txt: minMax.map(x => <>{x}</>).reduce((a,b) => <><a/><br/><b/></>, <></>)
+    }
+}
+
+const SmallFields : React.FC<{color: string, fieldname : ReactNode}> = ({color, fieldname, children}) => <div style={{flex: "1 1 auto"}} className='short-field mr-[5vw] mt-8'>
     <div style={{color}} className="font-[700] text-[22px]">{fieldname}</div>
     <div className="mt-2">{children}</div>
     <div className="mt-6" style={{ width: "74px", borderTop: "1px solid rgba(206, 206, 206, 0.2)" }}></div>
 </div>
+
+const BigFields : React.FC<{color: string, fieldname : ReactNode}> = ({color, fieldname, children}) => <div
+    style={{flex: "1 1 100%", background: "#353434"}}
+    className='short-field p-[18px]  mt-8'
+>
+    <div style={{color}} className="font-[700] text-[22px]">{fieldname}</div>
+    <div className="mt-2">{children}</div>
+</div>
+
 
 export default CardDetailsInno;
