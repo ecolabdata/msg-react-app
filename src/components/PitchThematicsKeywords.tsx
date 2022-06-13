6df0036647639c3e4e3392f42689ab7076215da3
Datebayo350 <istrateconstantin6@gmail.com>
2022-06-13 10:08:22 +0200

    PitchThematicsKeyWords component style and responsive improved on ListResearchResultAides component pages - WIP

--- src/components/PitchThematicsKeywords.tsx
+++ src/components/PitchThematicsKeywords.tsx
@@ -1,6 +1,4 @@
-import RocketLogo from './../assets/icons/Rockett.svg';
-import KeywordsLogo from './../assets/icons/Keywords.svg';
-import ThematicsLogo from './../assets/icons/Thematics.svg';
+import {useState} from 'react';
 import SelectInputOptions from './customComponents/SelectInputOptions';
 import { ThematicsEnum } from '../model/ThematicsEnum';
 import KeyWordsLabel from './dsfrComponents/KeyWordsLabel';
@@ -9,74 +7,99 @@ type PitchThematicsKeywordsParams = {
     usedDescription: [string, React.Dispatch<React.SetStateAction<string>>]
     usedSecteurs: [string[], React.Dispatch<React.SetStateAction<string[]>>]
     usedMotsClef: [string[], React.Dispatch<React.SetStateAction<string[]>>]
+    usedInListPage: boolean
 }
 
-const OverlappingTitle: React.FC<{ logo: string, txt: string }> = ({ logo, txt }) => <>
-    <div className="titleContainer absolute flex h-0 top-[-1em] overflow-visible">
-        <div className="flex h-fit w-full">
-            <img className="h-fit" src={logo} alt="Logo" />
-            <h2 className="italic text-dark-text-action text-xl md:text-3xl font-[Spectral]">{txt}</h2>
-        </div>
-    </div>
-    <div className="spacer mt-4 md:mt-8"></div>
-</>
+const OverlappingTitle: React.FC<{txt: string, usedInListPage:boolean, number: number}> = ({txt, usedInListPage, number }) => {
 
-export const PitchThematicsKeywords: React.FC<PitchThematicsKeywordsParams> = ({ usedDescription, usedSecteurs, usedMotsClef }) => {
+    return <>
+
+        <div className={`titleContainer ${!usedInListPage && 'absolute h-0 top-[-1em] left-4 overflow-visible'}`}>
+            <div className="flex w-fit">
+                <h2 className="text-xl italic text-dark-text-action font-[Spectral] flex items-baseline
+                md:text-3xl">{number}  <span className="mx-2 elipsis"></span>  {txt}</h2>
+            </div>
+        </div>
+    </>
+};
+
+export const PitchThematicsKeywords: React.FC<PitchThematicsKeywordsParams> = ({ usedDescription, usedSecteurs, usedMotsClef, usedInListPage }) => {
     const [description, setDescription] = usedDescription
     const [secteurs, setSecteurs] = usedSecteurs
     const [motsclefs, setMotsclef] = usedMotsClef
     const thematicsValues = Object.values(ThematicsEnum);
-    return <>
-        <div className='leftSideForm flex justify-center items-stretch basis-[50%] flex-shrink flex-grow'>
-            <div className={"flex flex-col items-around justify-center items-center  bg-background-form relative m-2"}>
-                <OverlappingTitle logo={RocketLogo} txt={"1. Votre projet"} />
-                <div className="flex flex-col items-stretch">
-                    <div className="fieldsContainer m-4 flex flex-col items-stretch">
-                        <p className="text-base text-center m-2">Décrivez en quelques lignes votre projet (thématique, technologie, cible, apports... ) pour obtenir des pistes pertinentes.</p>
-                        <textarea onChange={e => setDescription(e.target.value)} value={description} form="keywordsForm"
-                            className="cursor-text my-8 min-h-[225px] rounded-t-sm mt-4 addBorder-b border-3 border-gray-300 bg-background-inputs"
-                            placeholder="Expl. : “start-up de méthanisation” ou “nous sommes une startup spécialisée dans le processus biologique de dégradation des matières organiques...”" />
-                    </div>
-                </div>
-            </div>
-        </div>
+    const [collapsePitchContainer, setCollapsePitchContainer] = useState(true);
+    return (
+        <div className={`${usedInListPage && 'addBorder border border-dark-text-action bg-background-form'}
+        ${collapsePitchContainer ?'h-[40px] justify-center items-center' : 'lg:h-[408px]'}
+         relative container min-w-full flex flex-col items-end
+         lg:flex-row`}>
 
-        <div className="rightSideForm flex flex-col items-stretch  basis-[50%] flex-shrink flex-grow">
-            
-            <div className='thematicsContainer flex flex-col items-center bg-background-form relative m-2'>
-                <OverlappingTitle logo={ThematicsLogo} txt={"2. La thématique"} />
-                <div className="flex flex-col items-stretch">
-                    <div className="fieldsContainer m-4 flex flex-col items-stretch">
-                        <div className='fieldsContainer m-4 flex flex-col items-stretch'>
-                            <SelectInputOptions optionsData={thematicsValues} secteurs={secteurs} setSecteurs={setSecteurs} />
+             { collapsePitchContainer 
+                ?
+                <button type="button" onClick={ () => setCollapsePitchContainer(!collapsePitchContainer)} className="w-full text-dark-text-action flex justify-center items-center">Modifier la recherche <span className="ml-2">+</span></button>
+                : <>
+                    <button type="button" className="text-dark-text-action absolute top-0 right-10 m-2 w-[7%] flex justify-around items-center" onClick={() => {setCollapsePitchContainer(!collapsePitchContainer)}} > <span className="underline">Fermer</span><span className="rotate-45 block ml-2 text-3xl">+</span> </button>
+                    
+                    <div className='leftSideForm mb-8 h-[75%] w-full flex justify-center
+                    lg:w-1/2 '>
+                        
+                        <div className="projectCopntainer relative mt-8 h-fit w-[92%] flex flex-col justify-center
+                        lg:mt-0">
+
+                            <OverlappingTitle usedInListPage={usedInListPage} number={1} txt="Le projet" />
+                                
+                            <div className="fieldsContainer mt-2 flex flex-col w-full">
+                                <p className="text-base">Décrivez votre projet en quelques lignes.</p>
+                                <textarea onChange={e => setDescription(e.target.value)} value={description} form="keywordsForm"
+                                    className="mt-2 w-full rounded-t-sm addBorder-b border-3 border-white p-2 bg-background-inputs
+                                    lg:min-h-[212px]"/>
+                            </div>
+
                         </div>
-                    </div>
-                </div>
-            </div>
 
-            <div className="keyWordsContainer mt-8 flex flex-col items-center  relative bg-background-form m-2">
-                <OverlappingTitle logo={KeywordsLogo} txt="3. Les mots clés" />
-                <div className="w-full flex flex-col items-stretch">
-                    <div className="fieldsContainer m-4 flex flex-col items-stretch">
-                        <p className="text-base text-center">Ajoutez des mots clés représentatifs de votre activité. (facultatif)</p>
-                        <textarea
-                            onChange={e => {
-                                const motsclefs = e.target.value.split(",").map(x => x.trim()).filter(x => x)
-                                setMotsclef(motsclefs)
-                            }}
-                            className="cursor-text rounded-t-sm mt-4 h-15 addBorder-b border-3 border-gray-300 bg-background-inputs overflow-hidden resize-none"
-                            defaultValue={motsclefs.join(", ")}
-                        >
-                        </textarea>
-
-                        <ul className="keyWordsList h-[52px] mt-2 flex flex-wrap">
-                            {motsclefs.map(word => <li key={word}> <KeyWordsLabel keyWord={word} /> </li>)}
-                        </ul>
                     </div>
-                </div>
-            </div>
+
+                    <div className="rightSideForm mb-8 h-[75%] w-full flex flex-col items-center justify-between
+                    lg:w-1/2">
+                        
+                        <div className='thematicsContainer relative w-[92%] h-[50%] -background-form'>
+                            <OverlappingTitle usedInListPage={usedInListPage} number={2} txt="La thématique" />
+                            <div className="h-fit w-full">
+                                <div className='fieldsContainer mt-2 flex flex-col'>
+                                    <SelectInputOptions optionsData={thematicsValues} secteurs={secteurs} setSecteurs={setSecteurs} />
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="keyWordsContainer relative mt-8 max-h-full w-[92%] -background-form
+                        lg:mt-6">
+
+                            <OverlappingTitle usedInListPage={usedInListPage} number={3} txt="Les mots clés" />
+
+                                <p className="mt-4 text-base">Ajoutez des mots clés représentatifs de votre activité. (facultatif)</p>
+
+                                <textarea
+                                    onChange={e => {
+                                        const motsclefs = e.target.value.split(",").map(x => x.trim()).filter(x => x)
+                                        setMotsclef(motsclefs)
+                                    }}
+                                    className="cursor-text rounded-t-sm h-10 mt-2 w-full addBorder-b border-3 border-white overflow-hidden bg-background-inputs"
+                                    defaultValue={motsclefs.join(", ")}
+                                >
+                                </textarea>
+
+                                <ul className="keyWordsList max-h-[40px] mt-2 overflow-auto flex flex-wrap">
+                                    {motsclefs.map(word => <li key={word}> <KeyWordsLabel keyWord={word}/> </li>)}
+                                </ul>
+
+                        </div> 
+
+                    </div>
+                </>
+            }
 
         </div>
-
-    </>
+    )
 }
--- src/components/anonymous/ListResearchResultAides.tsx
+++ src/components/anonymous/ListResearchResultAides.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AidesClientQuery, AidesQuery, AnyCard, Search, searchAidesClient, searchAidesInno, searchInvestisseur } from '../../api/Api';
 import { useTitle } from '../../hooks/useTitle';
@@ -9,6 +9,7 @@ import ResultPreviewCard from '../customComponents/ResultPreviewCard';
 import ToggleButton from '../dsfrComponents/ToggleButton';
 import Pagination from '../dsfrComponents/Pagination';
 import ArrowDark from './../../assets/icons/arrow-dark-action.svg';
+import { Filtrer } from './../../assets/Icons';
 import all_aides_types from "../../api/aide-types.json"
 import Select from '../dsfrComponents/Select';
 import { PitchThematicsKeywords } from '../PitchThematicsKeywords';
@@ -35,35 +36,44 @@ const echeances: Record<string, number> = {
 }
 
 const ListResearchResultAides: React.FC<{ cardType: CardType }> = ({ cardType }) => {
-    const { usedCorbeille, usedNextScrollTarget } = useContext(ApplicationContext)
-    const [toggleInCorbeille, isInCorbeille] = usedCorbeille
-    const [nextScrollTarget, setNextScrolTarget] = usedNextScrollTarget
+    
+   
+    const { color } = cardType;
+    console.log('typeof color :>> ', typeof color);
+    const { usedCorbeille, usedNextScrollTarget } = useContext(ApplicationContext);
+    const [toggleInCorbeille, isInCorbeille] = usedCorbeille;
+    const [nextScrollTarget, setNextScrolTarget] = usedNextScrollTarget;
     const location = useLocation();
     const initialState = location.state as (InitialState & { page?: number, montantMin: number }) | null;
-    const initialQuery = initialState?.search.query as (AidesQuery | null)
+    const initialQuery = initialState?.search.query as (AidesQuery | null);
 
-    const pageNo = initialState?.page || 1
-    const navigate = useNavigate()
-    useTitle(`Recherche détaillé ${cardType.title}`)
+    const pageNo = initialState?.page || 1;
+    const navigate = useNavigate();
+    useTitle(`Recherche détaillé ${cardType.title}`);
 
     const [isLoading, setIsLoading] = useState(false)
-    const [description, setDescription] = useState(initialQuery?.description || "")
-    const [secteurs, setSecteurs] = useState<string[]>(initialQuery?.secteurs || [])
-    const [motsclefs, setMotsclef] = useState<string[]>(initialQuery?.motsclefs || [])
+    const [description, setDescription] = useState(initialQuery?.description || "");
+    const [secteurs, setSecteurs] = useState<string[]>(initialQuery?.secteurs || []);
+    const [motsclefs, setMotsclef] = useState<string[]>(initialQuery?.motsclefs || []);
     const [errorTxt, setErrorTxt] = useState(<></>)
     const [toggles, setToggles] = useState({
-        "Afficher les aides permanentes": (initialQuery && initialQuery["Afficher les aides permanentes"] != undefined) ? initialQuery["Afficher les aides permanentes"] : true
-    })
-    const [aid_type, setAid_type] = useState(initialQuery?.aid_type || "")
-    const [echeance, setEcheance] = useState(initialQuery?.echeance || "")
+        "Afficher les aides permanentes": (initialQuery && initialQuery["Afficher les aides permanentes"] !== undefined) ? initialQuery["Afficher les aides permanentes"] : true
+    });
+    const [aid_type, setAid_type] = useState(initialQuery?.aid_type || "");
+    const [echeance, setEcheance] = useState(initialQuery?.echeance || "");
     const toggleKeys = Object.keys(toggles) as (keyof typeof toggles)[]
     const pageChunkSize = 20;
+
     const filteredCards: JSX.Element[] | undefined = useMemo(() => {
+
         console.log("Running use memo")
         if (initialState?.search.cards) {
+
             let allCards: AnyCard[] = cardType.name === "aides-innovations" ? initialState.search.cards.aides_innovation : initialState.search.cards.aides_clients
             allCards = allCards.filter(x => !isInCorbeille(x))
+
             if (aid_type) allCards = allCards.filter(x => x.aid_types?.includes(aid_type))
+
             if (echeance) {
                 allCards = allCards.filter(card => {
                     if (card.name === "Réaliser des projets de solidarité internationale d'accès à l'eau") debugger;
@@ -78,8 +88,11 @@ const ListResearchResultAides: React.FC<{ cardType: CardType }> = ({ cardType })
                     }
                 })
             }
+
             if (!toggles["Afficher les aides permanentes"]) allCards = allCards.filter(card => card.submission_deadline)
+
             return allCards
+
         } else {
             return []
         }
@@ -92,43 +105,53 @@ const ListResearchResultAides: React.FC<{ cardType: CardType }> = ({ cardType })
     )
 
     const handleOnSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+        
         event.preventDefault();
+
         if (description.length > 0) {
             setIsLoading(true)
             setErrorTxt(<></>)
-            if (cardType.name === "aides-clients") {
-                searchAidesClient({
-                    description,
-                    motsclefs,
-                    secteurs,
-                    "Afficher les aides permanentes": toggles["Afficher les aides permanentes"],
-                    aid_type,
-                    echeance
-
-                }).then((search) => {
-                    setIsLoading(false)
-                    return navigate(cardType.searchLink, {
-                        replace: true,
-                        state: { search }
+            
+            switch (cardType.name) {
+                
+                case "aides-clients":
+                    return searchAidesClient({
+                        description,
+                        motsclefs,
+                        secteurs,
+                        "Afficher les aides permanentes": toggles["Afficher les aides permanentes"],
+                        aid_type,
+                        echeance
+    
+                    }).then((search) => {
+                        setIsLoading(false)
+                        return navigate(cardType.searchLink, {
+                            replace: true,
+                            state: { search }
+                        })
                     })
-                })
-            } else if (cardType.name === "aides-innovations") {
-                searchAidesInno({
-                    description,
-                    motsclefs,
-                    secteurs,
-                    "Afficher les aides permanentes": toggles["Afficher les aides permanentes"],
-                    aid_type,
-                    echeance
 
-                }).then((search) => {
-                    setIsLoading(false)
-                    return navigate(cardType.searchLink, {
-                        replace: true,
-                        state: { search }
+                case "aides-innovations":
+                    return searchAidesInno({
+                        description,
+                        motsclefs,
+                        secteurs,
+                        "Afficher les aides permanentes": toggles["Afficher les aides permanentes"],
+                        aid_type,
+                        echeance
+    
+                    }).then((search) => {
+                        setIsLoading(false)
+                        return navigate(cardType.searchLink, {
+                            replace: true,
+                            state: { search }
+                        })
                     })
-                })
+
+                default : 
+                    return Promise.reject("Unknown card type")
             }
+
         } else {
             setErrorTxt(<p style={{ color: "hsla(0, 100%, 65%, 0.9)" }}>La description de l'entreprise est obligatoire</p>)
         }
@@ -136,20 +159,17 @@ const ListResearchResultAides: React.FC<{ cardType: CardType }> = ({ cardType })
 
     return (
         <>
-            <div className="headContainer
-            mt-10 mx-auto max-w-[1240px]
+            <div className="headContainer  container mt-10 mx-auto max-w-headerSize
             xl:mx-auto
             ">
 
-                <div className="flex">
-                    <button onClick={() => window.history.back()} className="ml-4 text-dark-text-action flex mt-4 rm-link-underline "> <img className="mr-2" src={ArrowDark} alt="Icone flèche" /> Retour </button>
-                </div>
+                <button onClick={() => window.history.back()} className="text-dark-text-action flex mt-4 rm-link-underline "> <img className="mr-2" src={ArrowDark} alt="Icone flèche" /> Retour </button>
 
-                <div className="cardTitle mt-10 ml-4 p-2 text-base">
+                <div className="cardTitleAndLogo mt-10 p-2 text-base">
 
-                    <h2 className="w-fit font-bold  text-2xl md:text-4xl">
+                    <h2 className="w-fit font-bold text-2xl md:text-4xl">
 
-                        <div className="flex items-center">
+                        <div className="flex items-center ">
                             <cardType.SVGLogo width="30" height="30" style={{ color: cardType.color }} /> &nbsp;
                             {cardType.title} &nbsp; <span className="bg-yellow md:text-3xl font-light">{`(${filteredCards.length})`}</span>
                         </div>
@@ -160,59 +180,92 @@ const ListResearchResultAides: React.FC<{ cardType: CardType }> = ({ cardType })
                 </div>
 
 
-                <div className=" flex flex-col items-center w-full">
-                    <form onSubmit={(event) => handleOnSubmitForm(event)} id="keywordsForm">
-                        <div className="m-auto flex justify-around mt-8 flex-wrap">
+                <div className="flex flex-col items-center w-full">
+                    
+                    <form onSubmit={(event) => handleOnSubmitForm(event)} id="keywordsForm" className="h-fit w-full">
+                        
+                        <div className="researchContainer m-auto flex justify-around flex-wrap">
+
                             <PitchThematicsKeywords
                                 usedDescription={[description, setDescription]}
                                 usedMotsClef={[motsclefs, setMotsclef]}
                                 usedSecteurs={[secteurs, setSecteurs]}
+                                usedInListPage={true}
                             />
+
                         </div>
-                        <div className="researchContainer max-w-[1240px] w-full p-6 flex flex-col bg-research-precision-container items-center  lg:p-1">
+                        
+                        <div className="specifyResearchContainer mt-2 min-h-[160px] max-w-headerSize w-full flex flex-col items-center justify-center  bg-research-precision-container
+                        lg:mt-2 lg:justify-center">
+                            
+                            <div className="specifyAndLogoContainer w-full ">
 
-                            <h2 className=" bold text-xl text-center mt-4" style={{ color: cardType.color }}>Preciser la recherche </h2>
+                                <h2 className={`mt-4 bold text-xl text-[${cardType.color}] flex justify-center items-center`}>
+                                    <Filtrer className="mr-6" width="20" height="20"/> 
+                                    Préciser la recherche 
+                                </h2>
 
-
-                            <div className="inputsContainer p-4 flex justify-center items-middle
-                            lg:justify-between lg:items-end
-                            xl:justify-center flex-wrap">
-                                {/* <div className="my-2 flex flex-col items-center lg:flex-row lg:mb-6">
-                                    <div className="inputNumber mr-6 flex flex-col font-light ">
-                                        <label className="mb-1 text-white text-base" htmlFor="montantKEuro">Montant min. en K€</label>
-                                        <input
-                                            className={`text-white rounded-t-md w-64 h-10 addBorder-b border-2 bg-input-background`}
-                                            style={{ borderColor: cardType.color }} type="number" id="montantKEuro"
-                                            defaultValue={montantMin.toString()}
-                                            onChange={e => setMontantMin(Number.parseInt(e.target.value))}
-                                        />
-                                    </div>
-                                </div> */}
-
-                                <Select classes="my-4" label="Nature de l'aide"
-                                    color={cardType.color}
-                                    defaultOption={"Toutes"}
-                                    optionsData={all_aides_types.results.map(x => x.name)} onChange={e => {
-                                        setAid_type(e.currentTarget.value)
-                                    }} />
-                                &nbsp;&nbsp;&nbsp;
-                                <Select classes="my-4" label="Echéance"
-                                    color={cardType.color}
-                                    defaultOption={"Toutes"}
-                                    optionsData={Object.keys(echeances)} onChange={e => {
-                                        setEcheance(e.currentTarget.value)
-                                    }} />
-                                <div className="toggleButtons w-fit flex flex-col
-                        lg:flex-row lg:mb-6">
-                                    {toggleKeys.map(x => <ToggleButton label={x} checked={toggles[x]} color={cardType.color} onChange={e => setToggles({ ...toggles, [x]: !toggles[x] })} />)}
-                                </div>
                             </div>
+
+                            <div className="inputsAndToggleContainer self-end flex items-end  justify-around w-full mt-2
+                            ">
+
+                                <div className="inputsContainer w-full mt-2 flex flex-col items-center justify-center   
+                                lg:h-fit lg:w-[85%] lg:flex-row">
+
+                                    <Select classes=" w-[93%] 
+                                    lg:mx-2 lg:max-w-[202px]" label="Nature de l'aide"
+                                        color={cardType.color}
+                                        defaultOption={"Toutes"}
+                                        optionsData={all_aides_types.results.map(x => x.name)} onChange={e => {
+                                            setAid_type(e.currentTarget.value)
+                                        }} />
+
+                                    <Select classes=" w-[93%]
+                                    lg:mx-2 lg:max-w-[202px]" label="Zone géographique ciblée"
+                                        color={cardType.color}
+                                        defaultOption={"Toutes"}
+                                        //? Value muste be changed 
+                                        optionsData={Object.keys(echeances)} onChange={e => {
+                                            setEcheance(e.currentTarget.value)
+                                        }} />
+
+                                    <Select classes=" w-[93%]
+                                    lg:mx-2 lg:max-w-[202px]" label="Echéance"
+                                        color={cardType.color}
+                                        defaultOption={"Toutes"}
+                                        optionsData={Object.keys(echeances)} onChange={e => {
+                                            setEcheance(e.currentTarget.value)
+                                        }} />
+
+                                    <div className="toggleButtonsContainer w-[93%] flex flex-col grow
+                                    lg:h-full lg:flex lg:flex-col lg:items-center lg:justify-center">
+                                        {toggleKeys.map(x => <ToggleButton label={x} checked={toggles[x]} color={cardType.color} onChange={e => setToggles({ ...toggles, [x]: !toggles[x] })} />)}
+                                    </div>
+
+                                </div>
+
+                            </div>
+
                         </div>
+
                     </form>
+                    
                     <div className='h-12 w-full flex justify-center items-center color'>
                         {errorTxt}
                     </div>
-                    <button form="keywordsForm" disabled={isLoading} className="mt-8 w-48 h-14 text-xl fr-btn fr-btn--primary capitalize" > <span className="mx-auto">{isLoading ? "Chargement..." : "rechercher !"}</span> </button>
+
+                    <div className="researchButtonsContainer mt-4 w-full flex justify-center">
+                        <button type="button" disabled={isLoading} className="mx-3 fr-btn fr-btn--sm underline fr-btn--tertiary-no-outline   
+                        "> <span className={`mx-auto`}>Réinitialiser</span> </button>
+
+                        <button form="keywordsForm" disabled={isLoading} className="mx-3 fr-btn fr-btn--sm fr-btn--primary 
+                        "> <span className={`mx-auto`}>{isLoading ? "Chargement..." : "Valider et rechercher"}</span> </button>
+
+
+                    </div>
+
+
                 </div>
             </div>
 
