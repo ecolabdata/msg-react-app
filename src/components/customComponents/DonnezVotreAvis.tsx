a1244aaffa587f8bbe83793895327c0b8ca9e59b
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2022-06-23 22:05:45 +0200

    Update donnez votre avis form link

--- src/components/customComponents/DonnezVotreAvis.tsx
+++ src/components/customComponents/DonnezVotreAvis.tsx
@@ -5,9 +5,9 @@ export const DonnezVotreAvis = () => {
     const payload = useJwtPayload();
     let href;
     if (payload?.userTestCampaign || payload?.name) {
-        href = `https://airtable.com/shrZ61LDZn0adF26a?prefill_auth-test-user-campaign=${payload.userTestCampaign}&prefill_auth-name=${payload.name}&hide_auth-test-user-campaign=true&hide_auth-name=true`
+        href = `https://airtable.com/shrhHngOUIL3Frwb5?prefill_auth-test-user-campaign=${payload.userTestCampaign}&prefill_auth-name=${payload.name}&hide_auth-test-user-campaign=true&hide_auth-name=true`
     } else {
-        href = 'https://airtable.com/shr2A0YbTvAwdrxub'
+        href = 'https://airtable.com/shrhHngOUIL3Frwb5'
     }
     return <a  className="avis rm-link-underline" href={href} target="_blank">
         Donnez votre avis !
