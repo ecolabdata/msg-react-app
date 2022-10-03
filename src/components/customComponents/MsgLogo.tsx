df82e69551b4fdf2c572c81fd27a487f18d76dcc
vincentgrassin <vincent.grassin@troopers.email>
2022-10-03 16:45:33 +0200

    fix: change aria on logo

--- src/components/customComponents/MsgLogo.tsx
+++ src/components/customComponents/MsgLogo.tsx
@@ -3,7 +3,7 @@ import LogoMSG from '../../assets/msg-icon.svg';
 const MsgLogo: React.FC = () => {
   return (
     <>
-      <img className="h-12 mt-1 mr-2" src={LogoMSG} alt="Icône d'indication, version beta " />
+      <img className="h-12 mt-1 mr-2" src={LogoMSG} alt="" aria-hidden={true} />
       <div className="fr-header__service">
         <p className="fr-header__service-title capitalize">
           mes services greentech
