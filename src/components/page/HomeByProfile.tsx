cd458e0e77222fe77d57570f612f7e795000edb5
yves <yves@troopers.email>
2022-11-14 18:16:10 +0100

    Change url to download doc and not open

--- src/components/page/HomeByProfile.tsx
+++ src/components/page/HomeByProfile.tsx
@@ -19,20 +19,17 @@ const HomeByProfile: React.FC<HomeByProfileProps> = ({ profile }) => {
       <section>
         <div
           className="container-title container max-w-headerSize mx-auto p-2 
-                flex flex-col items-center"
-        >
+                flex flex-col items-center">
           <h1
             className="mt-4 w-full font-bold text-4xl text-center 
                 md:max-w-[60%]
-                "
-          >
+                ">
             {pageContent.mainContent.title}
           </h1>
           <h2
             className="mt-8 text-center w-[65%] leading-7 
                 lg:max-w-[62%]
-                "
-          >
+                ">
             {' '}
             {pageContent.mainContent.description}
           </h2>
@@ -59,8 +56,7 @@ const HomeByProfile: React.FC<HomeByProfileProps> = ({ profile }) => {
       )}
       <section
         className="container-title container max-w-headerSize my-8 mx-auto p-2 
-              flex flex-col items-center"
-      >
+              flex flex-col items-center">
         <h2 className="my-8 w-full font-bold text-3xl text-center md:max-w-[70%]">
           Cela peut aussi vous intéresser
         </h2>
@@ -77,8 +73,7 @@ const HomeByProfile: React.FC<HomeByProfileProps> = ({ profile }) => {
       {profile === 'startup' && (
         <section
           className="container-title container max-w-headerSize my-8 mx-auto p-2 
-              flex flex-col items-center"
-        >
+              flex flex-col items-center">
           {' '}
           <h2 className="my-8 w-full font-bold text-3xl text-center md:max-w-[70%]">
             {pageContent.secondaryContent.title}
@@ -87,8 +82,7 @@ const HomeByProfile: React.FC<HomeByProfileProps> = ({ profile }) => {
             <p
               className="text-center w-[65%] leading-7 
                 lg:max-w-[62%]
-                "
-            >
+                ">
               {pageContent.secondaryContent.description}
             </p>
           )}
@@ -96,13 +90,10 @@ const HomeByProfile: React.FC<HomeByProfileProps> = ({ profile }) => {
       )}
       <div
         className="container-title container max-w-headerSize my-8 mx-auto p-2 
-              flex flex-col items-center"
-      >
+              flex flex-col items-center">
         <Link
           className="w-fit h-9 text-base my-8 text-dark-text-action p-1 rm-link-underline flex justify-center text-xl"
-          onClick={() => window.scrollTo(0, 0)}
-          to={pageContent.secondaryContent.extraUrl}
-        >
+          to={pageContent.secondaryContent.extraUrl}>
           {pageContent.secondaryContent.extraLink}
         </Link>
       </div>
@@ -129,7 +120,7 @@ const homePageContent = {
 
         cta: {
           name: "télécharger le template d'argumentaire",
-          url: 'https://docs.google.com/document/d/1tFbq0duIZauZPTekjfJXFPylT96LHmNL/edit',
+          url: 'https://drive.google.com/uc?export=download&id=1tFbq0duIZauZPTekjfJXFPylT96LHmNL',
           icon: 'download'
         },
         links: [
@@ -210,8 +201,7 @@ const homePageContent = {
             className="text-base my-8 text-dark-text-action p-1 rm-link-underline"
             href="https://airtable.com/shrY73vlVka9j2cg8"
             target="_blank"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             ce formulaire d’inscription
           </a>
           ! (Sous réserve de validation par nos équipes)
@@ -235,14 +225,6 @@ const homePageContent = {
         title: 'Communauté',
         description: 'Rejoignez Rapidd, la communauté des achats durables',
         links: [{ name: 'Rapidd', url: 'https://rapidd.developpement-durable.gouv.fr/' }]
-      },
-      {
-        title: 'Aide',
-        description:
-          'Obtenez un accompagnement pour intégrer des considérations environnementales dans vos  achats.',
-        links: [
-          { name: 'Le guichet vert', url: 'https://gipmaximilien.limesurvey.net/353211?lang=fr%20' }
-        ]
       },
       {
         title: 'Financements',
