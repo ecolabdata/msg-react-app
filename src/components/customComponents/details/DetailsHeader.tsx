c049ac4faa3a715f462cc2bfcc78d89107e192d8
mariegautron <marigaut2@gmail.com>
2022-12-05 12:00:28 +0100

    a11y: fix reflow

--- src/components/customComponents/details/DetailsHeader.tsx
+++ src/components/customComponents/details/DetailsHeader.tsx
@@ -19,7 +19,7 @@ const DetailsHeader: React.FC<DetailsHeaderProps> = ({ card }) => {
 
   return (
     <>
-      <section className={`pt-8 px-[10%] min-h-[200px] mx-[-24px] ${backgroundColor}`}>
+      <section className={`pt-8 px-[10%] min-h-[200px] ${backgroundColor}`}>
         <p
           className={`fr-badge fr-badge--sm `}
           style={{ color: cardType?.color, backgroundColor: cardType?.backgroundColor }}
