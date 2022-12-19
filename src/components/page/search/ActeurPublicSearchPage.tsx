1165261f7399de8725deff751eb6fa6cff67a608
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2022-12-19 21:44:58 +0100

    Setup ActeurPublicSearchPage Miniature

--- src/components/page/search/ActeurPublicSearchPage.tsx
+++ src/components/page/search/ActeurPublicSearchPage.tsx
@@ -9,35 +9,45 @@ const SearchPageV2 = buildSearchPageV2(Api.searchActeurPublic)
 
 export const ActeurPublicSearchPage : React.FC<{cardType: CardType}> = ({cardType}) => {
     return <SearchPageV2 usedAdvancedFilter={useAdvancedFilters(cardType.name)} cardType={cardType}>
-        {(hit, i, isLoading) => <ActeurPublicResultCard key={i} isLoading={isLoading} ap={hit} />}
+        {(hit, i, isLoading) => <ActeurPublicResultCard key={i} isLoading={isLoading} ap={hit} cardType={cardType} />}
     </SearchPageV2>
 }
 
 interface ActeurPublicResultCardProps {
     isLoading?: boolean,
     ap: HitPublicBuyer,
+    cardType : CardType
 }
 
-export const ActeurPublicResultCard: React.FC<ActeurPublicResultCardProps> = ({ isLoading, ap }) => {
+export const ActeurPublicResultCard: React.FC<ActeurPublicResultCardProps> = ({ isLoading, ap, cardType }) => {
     const slug = slugify(ap.fields.public_actor_nom[0]);
+    const public_actor_id = ap.fields.public_actor_id[0]
+    const public_actor_id_siren = ap.fields.public_actor_id_siren[0]
+    const startups = ap.fields['labelled_startups.nom']
+    const decpFirst = Object.keys(ap.highlight).filter(key => key.startsWith("decp."))
+    const approchFirst = Object.keys(ap.highlight).filter(key => key.startsWith("approch_content."))
 
-    const nomHtml = ap.fields["public_actor_nom"][0]
-    const glHtml = Object.keys(ap.highlight).map(fieldname => <div key={fieldname} >
-        {(ap as any).highlight[fieldname].length} {fieldname} contiennent des mots de votre recherche
-        {/* <div className="brightness-75">{fieldname} :</div> */}
-        {/* <p style={{ width: "50%" }} dangerouslySetInnerHTML={{ __html: replaceHlTxt((ap as any).highlight[fieldname]) }}></p> */}
-    </div>)
+    const public_actor_nom = ap.fields.public_actor_nom[0]
+    const hlPublic_actor_nom = ap.highlight.public_actor_nom
+    ? <p dangerouslySetInnerHTML={{ __html: replaceHlTxt(ap.highlight.public_actor_nom[0], cardType.color) }}></p>
+    : undefined
 
-    return <ResultCard cardType={acheteurPublic} name={ap.fields.public_actor_nom[0]} toprow={'Ville / Région'} linkData={{}} slug={slug} isLoading={isLoading}>
-        <div>{ap._score}</div>
-        <div>{glHtml}</div>
+    return <ResultCard cardType={acheteurPublic} name={public_actor_nom} nameElem={hlPublic_actor_nom} toprow={'Ville / Région'} linkData={{}} slug={slug} isLoading={isLoading}>
+        {/* <div>{ap._score}</div>
+        <div>{public_actor_id}</div>
+        <div>{public_actor_id_siren}</div> */}
+        {startups && <div>Ils ont travaillé avec {startups?.join(',')}</div>}
+        {decpFirst.length  ? <><b style={{color: cardType.color}}>{decpFirst.length}</b> Marchés passés</> : null}
+        {(decpFirst.length && approchFirst.length) ? " et " : null}
+        {approchFirst.length  ? <><b style={{color: cardType.color}}>{approchFirst.length}</b> Achats programmés</> : null}
+        {(decpFirst.length || approchFirst.length) ? <div>contiennent des mots de votre recherche</div> : null}
     </ResultCard >
 }
 
 const openingTag = "@msg-highlighted-field@"
 const closingTag = '@/msg-highlighted-field@'
 
-function replaceHlTxt(txt : string) {
-    console.log({txt})
-    return txt.replaceAll("@msg-highlighted-field@", '<b style="color: green">').replaceAll('@/msg-highlighted-field@', '</b>')
+function replaceHlTxt(txt: string, cssColor: string) {
+  console.log({ txt })
+  return txt.replaceAll(openingTag, `<b style="color: ${cssColor}">`).replaceAll(closingTag, '</b>')
 }
